b88e872d255e7656d8941ff22327703e77c83954
NP-Vincent <n.p.vincent@icloud.com>
2026-01-02 06:54:38 +0400

    Create contract console UI

--- src/wallet/appKit.js
+++ src/wallet/appKit.js
@@ -1,7 +1,10 @@
-import { sepolia, mainnet } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit'
+import { mainnet, sepolia } from '@reown/appkit/networks'
 
-const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+const projectId =
+  import.meta.env.VITE_PROJECT_ID ||
+  'b56e18d47c72ab683b10814fe9495694'
+
 if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set')
 }
@@ -17,6 +20,6 @@ export const appKit = createAppKit({
     email: false
   },
   themeVariables: {
-    '--w3m-accent': '#000000',
+    '--w3m-accent': '#2b2f3a'
   }
 })
