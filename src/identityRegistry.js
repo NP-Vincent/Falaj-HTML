557d2de998d27ac90b1ad8f6895f3171b5f85cf9
NP-Vincent <n.p.vincent@icloud.com>
2026-01-03 05:28:26 +0400

    Add Identity Registry interface

--- src/identityRegistry.js
+++ src/identityRegistry.js
@@ -37,6 +37,11 @@ export async function participantCount(mmProvider) {
   return contract.participantCount();
 }
 
+export async function getParticipant(mmProvider, account) {
+  const contract = getIdentityRegistryContract(mmProvider);
+  return contract.getParticipant(account);
+}
+
 export async function getRoleAdmin(mmProvider, role) {
   const contract = getIdentityRegistryContract(mmProvider);
   return contract.getRoleAdmin(role);
@@ -47,7 +52,57 @@ export async function hasRole(mmProvider, role, account) {
   return contract.hasRole(role, account);
 }
 
+export async function hasParticipantRole(mmProvider, role, account) {
+  const contract = getIdentityRegistryContract(mmProvider);
+  return contract.hasParticipantRole(role, account);
+}
+
+export async function isAllowedToTransact(mmProvider, account) {
+  const contract = getIdentityRegistryContract(mmProvider);
+  return contract.isAllowedToTransact(account);
+}
+
+export async function addParticipant(mmProvider, account, role, kycExpiry) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.addParticipant(account, role, kycExpiry);
+}
+
+export async function changeRole(mmProvider, account, role) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.changeRole(account, role);
+}
+
 export async function freezeAccount(mmProvider, account, reason) {
   const contract = await getIdentityRegistryContractWithSigner(mmProvider);
   return contract.freezeAccount(account, reason);
+}
+
+export async function unfreezeAccount(mmProvider, account) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.unfreezeAccount(account);
+}
+
+export async function removeParticipant(mmProvider, account) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.removeParticipant(account);
+}
+
+export async function renewKYC(mmProvider, account, newExpiry) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.renewKYC(account, newExpiry);
+}
+
+export async function pause(mmProvider) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.pause();
+}
+
+export async function unpause(mmProvider) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.unpause();
+}
+
+export async function setPrecompileSync(mmProvider, enabled) {
+  const contract = await getIdentityRegistryContractWithSigner(mmProvider);
+  return contract.setPrecompileSync(enabled);
 }
