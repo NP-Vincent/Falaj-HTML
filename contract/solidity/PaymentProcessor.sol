b880b8a353eea300204ec5df44822e25730825eb
NP-Vincent <n.p.vincent@icloud.com>
2026-01-08 02:43:52 +0400

    Add local teleporter interfaces

--- contract/solidity/PaymentProcessor.sol
+++ contract/solidity/PaymentProcessor.sol
@@ -8,30 +8,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-/**
- * @title ITeleporterMessenger
- * @notice Interface for Teleporter cross-chain messaging
- */
-interface ITeleporterMessenger {
-    struct TeleporterMessageInput {
-        bytes32 destinationChainId;
-        address destinationAddress;
-        bytes message;
-        uint256 requiredGasLimit;
-        uint256 fee;
-        address[] allowedRelayerAddresses;
-    }
-
-    /**
-     * @notice Send a cross-chain Teleporter message
-     * @param messageInput Message input parameters for the Teleporter message
-     * @return messageId The unique identifier for the sent message
-     */
-    function sendCrossChainMessage(
-        TeleporterMessageInput calldata messageInput
-    ) external payable returns (bytes32 messageId);
-}
+import "./teleporter/ITeleporterMessenger.sol";
 
 /**
  * @title PaymentProcessor
